4147ce2661e1d569c91141d87c9a06c96e141248
MV powerappmath <gabrielantoniassi12@gmail.com>
2020-11-05 18:33:11 -0300

    h

--- matrix1.h
+++ matrix1.h
@@ -76,7 +76,7 @@ public:
                       mRHSrow11, mRHSrow21, mRHSrow31, mRHSrow41,
                       mRHSrow12, mRHSrow22, mRHSrow32, mRHSrow42;
 
-      if (EStorageOrder == RowMajor)
+      if (StorageOrder == RowMajor)
       {
               //{ [a11 a12 a13 a14], [a21 a22 a23 a24], [a31 a32 a33 a34], [a41 a42 a43 a44]}
 
