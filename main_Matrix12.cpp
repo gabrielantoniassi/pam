6aa64351df8854f227f9d5cf70282a2dc9410f49
Ubuntu <gabrielantoniassi12@gmail.com>
2020-12-03 15:12:42 +0000

    cleaning

--- main_Matrix12.cpp
+++ main_Matrix12.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "Matrix_12inside.h"
-using namespace std;
 
 int main() {
 
-	SMatrix<double, 12, 12> m1, m2;
+	SMatrix<double, 12, 12, ColMajor> m1;
+	SMatrix<double, 12, 12> m2; 	
 
 	for (int i = 0; i < 12*12; i++) {
 		m1.m_data[i] = i + 1;
@@ -13,7 +13,7 @@ int main() {
 
 	SMatrix<double, 12, 12> m3 = m1.mul<12, 12>(m2);
 
-	cout << m3;
+	std::cout << m3;
 
 	return 0;
 }
